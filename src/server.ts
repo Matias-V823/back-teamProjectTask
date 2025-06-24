3e82131f31f3489e8458a1a55f02547fb198733f
Matias-V823 <matias.varas62@gmail.com>
2025-06-24 12:03:58 -0400

    add connection to BD mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import { connectDB } from "./config/db";
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+
+connectDB()
 const app = express()
 
 export default app
