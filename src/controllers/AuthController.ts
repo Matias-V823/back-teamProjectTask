076cebde0ad3fea745031a474e6c9e75e0820b51
Matias-V823 <matias.varas62@gmail.com>
2025-07-12 11:57:19 -0400

    I implemented hash to password of Users

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,12 +1,16 @@
-import colors from 'colors'
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
+import bcrypt from 'bcrypt'
 import User from '../models/User';
+import colors from 'colors'
+import { hashPassword } from '../utils/auth';
 
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         try {
+            const { password } = req.body
             const user = new User(req.body)
+            user.password = await hashPassword(password)
             await user.save()
             res.status(201).json({ message: 'Cuenta creada, revisa tu email para confirmarla.' });
         } catch (error) {
